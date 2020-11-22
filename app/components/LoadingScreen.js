cd663affa0b8081d62efb4f8dd32b1497b61771e
Aung <aungmyintstar.11@gmail.com>
2020-11-22 08:07:05 +0630

    Initial setup

---
+++ app/components/LoadingScreen.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AppLoading } from "expo";
+
+import SampleData from "../redux/sample.json";
+
+//1 - LOADING SCREEN
+export default function LoadingScreen(props) {
+  useEffect(() => checkLocalData(), []);
+
+  function checkLocalData() {
+    //Check if LocalStorage has been populated with the sample data
+    AsyncStorage.getItem("quotes", (err, data) => {
+      //if it doesn't exist, extract from json fil
+      if (data === null) {
+        AsyncStorage.setItem("quotes", JSON.stringify(SampleData.quotes)); //save the initial data in Async
+
+        props.navigation.navigate("App"); //Navigate to the home page
+      } else {
+        props.navigation.navigate("App"); //Navigate to the home page
+      }
+    });
+  }
+
+  return <AppLoading />;
+}
